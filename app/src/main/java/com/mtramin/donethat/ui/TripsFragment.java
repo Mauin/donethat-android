9966dc79c8b7afa14730355e51ae5239f08a0179
Marvin Ramin <m.ramin@mytaxi.net>
2015-09-02 07:49:35 +0200

    fixes crash in Trips List

--- app/src/main/java/com/mtramin/donethat/ui/TripsFragment.java
+++ app/src/main/java/com/mtramin/donethat/ui/TripsFragment.java
@@ -103,7 +103,7 @@ public class TripsFragment extends BaseFragment {
                         .observeOn(AndroidSchedulers.mainThread())
                         .startWith(database.getTrips())
                         .subscribe(
-                                adapter::setData, // TODO here be a crash (inconsistency detected)
+                                adapter::setData,
                                 throwable -> LogUtil.logException(this, throwable)
                         )
         );
