67fd016a90df106134970cebd1284f6bcafa08c5
Mauin <marvin.ramin@gmail.com>
2015-09-27 13:56:55 +0200

    test setup

---
+++ app/src/androidTest/java/com/mtramin/donethat/SyncServiceTest.java
@@ -0,0 +1,19 @@
+package com.mtramin.donethat;
+
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.MediumTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * TODO: JAVADOC
+ */
+@RunWith(AndroidJUnit4.class)
+@MediumTest
+public class SyncServiceTest {
+
+    @Test
+    public void testOnlyDownloadSync() {
+    }
+}
